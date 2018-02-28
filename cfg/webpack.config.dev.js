6ae908b57caa67acb8e05432a87a771bc38b3d37
unknown <gaopengfei@mvwchina.com>
2018-02-28 09:35:37 +0800

    css模块化配置

--- cfg/webpack.config.dev.js
+++ cfg/webpack.config.dev.js
@@ -1,9 +1,13 @@
 // 开发环境打包配置
 
+const path = require('path');
 const base = require('./webpack.config.base')
 const dfPath = require('./path')
 // webpack配置合并工具
 const merge =require('webpack-merge')
+
+
+const RS = (...arg)=>path.resolve( __dirname , ...arg )
 
 // 合并方式配置
 let strategyMerge = merge.strategy({
@@ -20,11 +24,41 @@ let config = {
                     dfPath.node_modules
                 ]
             },
+            // {
+            //     test: /\.css$/,
+            //     use: ['style-loader', 'css-loader']
+            // },
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                test:/\.css$/,
+                use:[
+                    'style-loader',
+                    {
+                        loader:'css-loader',
+                        options:{
+                            // css模块化，方便多人开发
+                            module:true,
+                            // 定义模块化css后的类名（默认为hash值，可读性差）path:路劲； name：文件名； local：本地定义的className
+                            localIdentName: '[path][name]__[local]--[hash:base64:5]'
+                        },
+                    }
+                ],
+                // 排除的文件，遇到这些文件不会用 loader 处理，也就不会模块化
+                exclude:[
+                    RS('./src/common'),                  
+                    RS('node_modules')
+                ]
             },
-			{
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader'],
+                include:[
+                    RS('./src/common'),                  
+                    RS('node_modules')
+                ]
+                
+            },            
+            
+            {
                 test: /\.(png|jpg|jpeg|gif)$/,
                 use: ['url-loader?limit=8192'],
             },
@@ -52,6 +86,12 @@ let config = {
     },
 
     devtool: 'cheap-module-eval-source-map',
+
+    devServer:{
+        // 服务器打包后，输出的资源路劲
+        publicPath:'/',
+        open:true
+    }
 };
 
 // 导出合并后的webpack配置
